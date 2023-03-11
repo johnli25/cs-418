b53c2977802497f9f0e68cd1bf924998419c1a94
johnli25 <warriorjohn36@gmail.com>
2023-03-11 03:42:40 -0600

    added function comments

--- mp_warmup2/mp2.js
+++ mp_warmup2/mp2.js
@@ -1,6 +1,11 @@
 // import * as math from "./math.js";
+
+/** @global global vertex buffer CPU-based vertex movement */
 var vertexBufGlobal;
 
+/** compiles and links GLSL to rest of program and graphics 
+ * @param {vs_source, fs_source}
+*/
 function compileAndLinkGLSL(vs_source, fs_source) {
     let vs = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vs, vs_source)
@@ -28,29 +33,71 @@ function compileAndLinkGLSL(vs_source, fs_source) {
     }
 }
 
-function supplyDataBuffer(data, program, vsIn, mode) {
-    if (mode === undefined) mode = gl.STATIC_DRAW
+// function supplyDataBuffer(data, program, vsIn, mode) {
+//     if (mode === undefined) mode = gl.STATIC_DRAW
     
-    let buf = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, buf)
-    let f32 = new Float32Array(data.flat())
-    gl.bufferData(gl.ARRAY_BUFFER, f32, mode)
+//     let buf = gl.createBuffer()
+//     gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+//     let f32 = new Float32Array(data.flat())
+//     gl.bufferData(gl.ARRAY_BUFFER, f32, mode)
     
-    let loc = gl.getAttribLocation(program, vsIn)
-    gl.vertexAttribPointer(loc, data[0].length, gl.FLOAT, false, 0, 0)
-    gl.enableVertexAttribArray(loc)
+//     let loc = gl.getAttribLocation(program, vsIn)
+//     gl.vertexAttribPointer(loc, data[0].length, gl.FLOAT, false, 0, 0)
+//     gl.enableVertexAttribArray(loc)
     
-    return buf;
+//     return buf;
+// }
+
+/** optional part: setup ILLINI LOGO GEOMETRY for cpu-based vertex movement 
+ * @param {geom}
+*/
+function setupCPUVertexBased(geom) {
+    var triangleArray = gl.createVertexArray()
+    gl.bindVertexArray(triangleArray)
+
+    // Object.entries({k1:v1, k2:v2}) returns [[k1,v1],[k2,v2]]
+    // [a, b, c].forEach(func) calls func(a), then func(b), then func(c)
+    Object.entries(geom.attributes).forEach(([name,data]) => {
+        // goal 1: get data from CPU memory to GPU memory 
+        // createBuffer allocates an array of GPU memory
+        let buf = gl.createBuffer()
+        // to get data into the array we tell the GPU which buffer to use
+        gl.bindBuffer(gl.ARRAY_BUFFER, buf)
+        // and convert the data to a known fixed-sized type
+        let f32 = new Float32Array(data.flat())
+        // then send that data to the GPU, with a hint that we don't plan to change it very often
+        gl.bufferData(gl.ARRAY_BUFFER, f32, gl.STATIC_DRAW)
+        
+        // goal 2: connect the buffer to an input of the vertex shader
+        // this is done by finding the index of the given input name
+        let loc = gl.getAttribLocation(program, name)
+        // telling the GPU how to parse the bytes of the array
+        gl.vertexAttribPointer(loc, data[0].length, gl.FLOAT, false, 0, 0)
+        // and connecting the currently-used array to the VS input
+        gl.enableVertexAttribArray(loc)
+    })
+
+    // We also have to explain how values are connected into shapes.
+    // There are other ways, but we'll use indices into the other arrays
+    var indices = new Uint16Array(geom.triangles.flat())
+    // we'll need a GPU array for the indices too
+    var indexBuffer = gl.createBuffer()
+    // but the GPU puts it in a different "ready" position, one for indices
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
+    // we return all the bits we'll need to use this work later
+    // return {
+    //     mode:gl.TRIANGLES,      // grab 3 indices per triangle
+    //     count:indices.length,   // out of this many indices overall
+    //     type:gl.UNSIGNED_SHORT, // each index is stored as a Uint16
+    //     vao:triangleArray       // and this VAO knows which buffers to use
+    // }
 }
 
-function setupCPUVertexBased() {
-    let buf = gl.createBuffer()
-    vertexBufGlobal = buf
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufGlobal)
-    let f32 = new Float32Array(data.flat())
-    gl.bufferData(gl.ARRAY_BUFFER, f32, gl.DYNAMIC_DRAW);
-}
-
+/** set up geometry for required/part 1
+ * @param {geom}
+*/
 function setupGeometry(geom) {
     // a "vertex array object" or VAO records various data provision commands
     var triangleArray = gl.createVertexArray()
@@ -104,7 +151,7 @@ function setupGeometry(geom) {
  *
  * Fills the screen with Illini Orange
  */
-function draw1(milliseconds) {
+function draw1() {
     gl.clearColor(1, 0.373, 0.02, 1)
     gl.clear(gl.COLOR_BUFFER_BIT)
     window.pending = requestAnimationFrame(draw1)
@@ -150,6 +197,11 @@ const m4mul = (...args) => args.reduce((m1,m2) => {
     return ans // m*m
 })
 
+/**
+ * Animation callback for the third display (required/part 1). See {draw1} for more.
+ * Scaling/Shrinking + Rotation
+ * @param {seconds}
+ */
 function draw3(seconds) {
     // gl.clearColor(1, 0.373, 0.02, 1)
     // gl.clear(gl.COLOR_BUFFER_BIT) 
@@ -169,9 +221,14 @@ function draw3(seconds) {
     window.pending = requestAnimationFrame(draw3)
 }
 
+/**
+ * Animation callback for fourth display. See {draw1} for more.
+ *
+ * Fills the screen with Illini Blue
+ */
 function draw4() {
     // gl.clear(gl.COLOR_BUFFER_BIT)
-    setupCPUVertexBased()
+    // setupCPUVertexBased()
     window.pending = requestAnimationFrame(draw4)
 }
 
@@ -194,6 +251,9 @@ function resizeCanvas() {
     else c.height = c.width
 }
 
+/** asynchrounous setup: build data & waiting for and linking fragment and vertex WebGL shaders 
+ * @parameter {event}
+*/
 async function setup(event) {
     window.gl = document.querySelector('canvas').getContext('webgl2')
     let vs = await fetch('vertex_shader_mp2.glsl').then(res => res.text())
@@ -202,7 +262,20 @@ async function setup(event) {
     let data = await fetch('illini.json').then(r=>r.json())
     console.log(data)
     window.geom = setupGeometry(data)
-    // requestAnimationFrame(draw3)
+}
+
+/** asynchrounous setup: build data & waiting for and linking fragment and vertex WebGL shaders 
+ * for CPU-based vertex animation only
+ * @parameter {event}
+*/
+async function setupCPU(event) {
+    window.gl = document.querySelector('canvas').getContext('webgl2')
+    let vs = await fetch('vertex_shader_mp2.glsl').then(res => res.text())
+    let fs = await fetch('fragment_shader_mp2.glsl').then(res => res.text())
+    compileAndLinkGLSL(vs,fs)
+    let data = await fetch('illini.json').then(r=>r.json())
+    console.log(data)
+    // window.geom = setupCPUVertexBased(data) // HOW DO I INCORPORATE THIS SUCCESSFULLY?????
 }
 
 /**
@@ -216,6 +289,7 @@ window.addEventListener('load',(event)=>{
     resizeCanvas()
     window.gl = document.querySelector('canvas').getContext('webgl2')
     setup()
+    setupCPU()
     document.querySelectorAll('input[name="example"]').forEach(elem => {
         elem.addEventListener('change', radioChanged)
     })
