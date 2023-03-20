e9b2ee82455953f1785292dfccc530ce80dcd110
johnli25 <warriorjohn36@gmail.com>
2023-03-20 17:15:28 -0500

    working state lesgo!

---
+++ mp_warmup2/cpu_vertex_shader.glsl
@@ -0,0 +1,19 @@
+#version 300 es
+
+in vec4 position;
+in vec4 color;
+
+uniform float seconds;
+
+out vec4 vColor;
+
+uniform mat4 combined_mat;
+
+void main() {
+    // gl_Position = vec4(
+    //     position.xy*cos(float(gl_VertexID) * seconds*1.1),
+    //     position.zw
+    // );
+    gl_Position = position;
+    vColor = color;
+}
